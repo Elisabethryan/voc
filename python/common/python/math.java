0eb012d4f60854730055edbf3df48d79615ad09e
elisabethryan <ebethryan@gmail.com>
2020-09-13 13:59:06 +0200

    set up the module?

---
+++ python/common/python/math.java
@@ -0,0 +1,29 @@
+package python;
+
+@org.python.Module(
+        __doc__ = "\math?\n"
+)
+public class math extends org.python.math.Module {
+    static {
+
+    }
+
+    private static long variable;
+
+    // @org.python.Attribute
+    // public static org.python.Object BuiltinFunctionType;
+
+    // @org.python.Attribute
+    // public static org.python.Object BuiltinMethodType;
+    @org.python.Attribute
+    public static org.python.Object attribute;
+
+
+    @org.python.Method(
+            __doc__ = ""
+    )
+    public static org.python.Object _math_function() {
+        throw new org.python.exceptions.NotImplementedError("math._math_function() has not been implemented.");
+    }
+
+}
