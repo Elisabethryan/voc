ef5145d15f96332a4020a47c726cc9bf870bf56a
Daniel Wang <Daniel.zhixi.wang@gmail.com>
2020-09-17 19:56:57 +0200

    Another bugfix.

--- python/common/python/math.java
+++ python/common/python/math.java
@@ -13,9 +13,9 @@ public class math extends org.python.types.Module {
         __doc__ = "Returns a the absolute value of the provided argument\n",
         args = {"x"}
     )
-    public static int abs(org.python.Object x) {
-        long abs = ((org.python.types.Int) x).value;
-        return (int) java.lang.Math.abs(abs);
+    public static double fabs(org.python.Object x) {
+        double abs = ((org.python.types.Float) x).value;
+        return Math.abs(abs);
     }
 
 }
