7cdca1f1e7ec9b6ac376b7b8daf3a8f36db5478b
Daniel Wang <Daniel.zhixi.wang@gmail.com>
2020-09-17 19:25:03 +0200

    correcting mistake

--- tests/stdlib/test_math.py
+++ tests/stdlib/test_math.py
@@ -1,8 +1,12 @@
+#import sys
+
+#sys.path.append("..")
+
 from ..utils import TranspileTestCase
 
 class MathModuleTests(TranspileTestCase):
     def test_floor(self):
         self.assertCodeExecution("""
             import math
-            print(math.abs(-5))
+            print(math.fabs(-5))
         """)
