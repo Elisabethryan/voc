ec58a4c9d5bc5ae6298489e47705e4059fc1a9e1
root <root@Daniel-PC.localdomain>
2020-09-17 02:54:47 +0200

    bugfix

--- python/common/python/math.java
+++ python/common/python/math.java
@@ -1,18 +1,22 @@
 package python;
 import java.lang.*;
+import org.python.types.Int;
 
 @org.python.Module(
         __doc__ =  "This module implements functions similar to those found in the math module in python\n"
 )
 public class math extends org.python.types.Module {
+    public math() {
+        super();
+    }
 
     @org.python.Method(
         __doc__ = "Returns a the absolute value of the provided argument\n",
         args = {"x"}
     )
     public static int fabs(org.python.Object x) {
-        int abs = ((org.python.types.Int) x).value;
-        return (int) Math.abs(num);
+        long abs = ((org.python.types.Int) x).value;
+        return (int) java.lang.Math.abs(abs);
     }
 
 }
