6c2c8526710e549989afd918252b0ed1c001798c
elisabethryan <ebethryan@gmail.com>
2020-09-24 14:57:10 +0200

    set up junit

---
+++ java_tests/TestTimeDelta.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import org.python.stdlib.datetime.DateTime;
+
+import junit.framework.TestCase;
+import datetime.java;
+//package org.python.stdlib.datetime;
+
+public class TestTimeDelta extends TestCase {
+  @Test
+  public void testTrue() {
+    assertTrue(DateTime.isSequence(other));
+  }
+}
